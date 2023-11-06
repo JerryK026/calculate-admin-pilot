fb75ee21a7af9affcd8bf3dc8de254f27762638e
SeokyungKim <kskyung0624@gmail.com>
2023-11-06 17:32:45 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,90 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.1.5"
+	id("io.spring.dependency-management") version "1.1.3"
+	id("org.asciidoctor.jvm.convert") version "3.3.2"
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
+	kotlin("plugin.jpa") version "1.8.22"
+}
+
+group = "com.pilot"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+val snippetsDir by extra { file("build/generated-snippets") }
+
+dependencies {
+	// kotlin
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// web
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	// data
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-jooq")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	runtimeOnly("com.h2database:h2")
+
+	// security
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	// test
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+
+	// docs
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	outputs.dir(snippetsDir)
+}
+
+tasks.asciidoctor {
+	inputs.dir(snippetsDir)
+	dependsOn(tasks.test)
+
+	doFirst {
+		delete {
+			file("src/main/resources/static/docs")
+		}
+	}
+}
+
+tasks.register("copyHTML", Copy::class) {
+	dependsOn(tasks.asciidoctor)
+	from(file("build/asciidoc/html5"))
+	into(file("src/main/resources/static/docs"))
+}
+
+tasks.build {
+	dependsOn(tasks.getByName("copyHTML"))
+}
+
+tasks.bootJar {
+	dependsOn(tasks.asciidoctor)
+	dependsOn(tasks.getByName("copyHTML"))
+}
+
---
+++ src/main/kotlin/com/pilot/calculateadmin/CalculateAdminApplication.kt
@@ -0,0 +1,11 @@
+package com.pilot.calculateadmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalculateAdminApplication
+
+fun main(args: Array<String>) {
+	runApplication<CalculateAdminApplication>(*args)
+}
